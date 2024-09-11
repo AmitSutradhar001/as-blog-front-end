ce33b8393bec9b93df4f5fe9c1568e0b6eeba3c1
AmitSutradhar001 <sutradharamit001@gmail.com>
2024-09-11 20:59:59 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,16 @@ export default defineConfig({
     host: "0.0.0.0", // Bind to 0.0.0.0 so it works in the cloud
     port: process.env.PORT || 3000, // Use the Render-provided port or fallback to 3000
   },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+            return "assets/[name]-[hash][extname]";
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
 });
